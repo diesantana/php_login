27dbf6489ff3398f087d5b78262939b816132811
Diego Santana <3c.santana@gmail.com>
2025-07-17 16:55:17 -0300

    feat: Adiciona a base de dados mock (users.php) para autenticação.

---
+++ inc/users.php
@@ -0,0 +1,16 @@
+<?php
+// Usuários
+return [
+    [
+        'user' => 'usuario1@gmail.com',
+        'password' => 'usuario001'
+    ],
+    [
+        'user' => 'usuario2@gmail.com',
+        'password' => 'usuario002'
+    ],
+    [
+        'user' => 'usuario3@gmail.com',
+        'password' => 'usuario003'
+    ]
+];
